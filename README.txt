9eb1d1b2357284e974bff2ce24bb3696a2914d66
Dw-Develops <36354410+Dw-Develops@users.noreply.github.com>
2019-01-19 20:53:12 -0500

    Files Uploaded
    Built on Codepen.Io for testing via #freeCodecamp for their #ResponsiveWebDesign Certification

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Dw-Develops/pen/LMgWzV.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,26 @@
+    
+
+//var x = document.getElementById("name").value;
+  //document.getElementById("demo").innerHTML = x;
+
+
+
+function submitAll() {
+let war = [
+  "Warrior",
+  "Viking",
+  "Knight",
+  "Paladin",
+  "Assassin",
+  "Samurai",
+  "Thief",
+  "Royalty"
+];  
+  
+  var x = document.getElementById("name").value;
+  var y = document.getElementById("email").value;
+  var z = document.getElementById("number").value;
+      
+  console.log(x + y + z + war[0]);
+
+};
